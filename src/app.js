119b7d50eae44755e1039794e1318d394c6307cf
Emiliano Quiñones <emidqtrabajo@gmail.com>
2026-07-29 19:57:03 -0300

    Entrega final Backend 3

--- src/app.js
+++ src/app.js
@@ -6,21 +6,35 @@ import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
 import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
-
 import mocksRouter from './routes/mocks.router.js';
 
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+
 const app = express();
-const PORT = process.env.PORT||8080;
-const connection = mongoose.connect(`mongodb+srv://Emi:emi123456@cluster0.d1txvpu.mongodb.net/ecommerce?retryWrites=true&w=majority`)
+const PORT = process.env.PORT || 8080;
+
+mongoose.connect("mongodb+srv://Emi:emi123456@cluster0.d1txvpu.mongodb.net/ecommerce?retryWrites=true&w=majority");
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/users',usersRouter);
-app.use('/api/pets',petsRouter);
-app.use('/api/adoptions',adoptionsRouter);
-app.use('/api/sessions',sessionsRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/pets', petsRouter);
+app.use('/api/adoptions', adoptionsRouter);
+app.use('/api/sessions', sessionsRouter);
+app.use('/api/mocks', mocksRouter);
 
-app.use('/api/mocks',mocksRouter);
+const swaggerDocument = YAML.load("./src/docs/Users.yaml");
 
-app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
+app.use(
+    "/apidocs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument)
+);
+
+const server = app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`);
+});
+
+export default app;
